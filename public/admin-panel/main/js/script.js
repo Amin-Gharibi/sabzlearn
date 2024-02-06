76758af6d3e31a396e5d077f5d75d56275d7a1e5
Amin Gharibi <gharibi8364@gmail.com>
2024-02-06 13:52:31 +0330

    show info boxes in the admin panel

--- public/admin-panel/main/js/script.js
+++ public/admin-panel/main/js/script.js
@@ -1,9 +1,46 @@
-import {getLatestSignedUpUsers} from "../../utils.js";
+import {getToken} from "../../../scripts/utils/utils.js";
 
-const users = (await getLatestSignedUpUsers()).slice(0, 10);
+const response = await fetch('http://localhost:4000/v1/infos/p-admin', {
+    headers: {
+        "Authorization": `Bearer ${getToken()}`
+    }
+})
+
+const infos = await response.json()
+
+const infoBoxesContainer = document.querySelector('.info-boxes-container')
+infos.infos.forEach(box => {
+    infoBoxesContainer.insertAdjacentHTML('beforeend',`
+        <div class="col-4">
+            <div class="home-content-revanue box">
+                <div class="home-box">
+                    <div class="home-box-left">
+                        <div class="home-box-title">
+                            <span>${box.title}</span>
+                        </div>
+                        <div class="home-box-value">
+                            <div class="home-box-price">
+                                <span>${box.count}</span>
+                            </div>
+                        </div>
+                        <div class="home-box-text">
+                            <span>تعداد ${box.title} بصورت کلی</span>
+                        </div>
+                    </div>
+                    <div class="home-box-right">
+                        <div class="home-box-icon">
+                            <i class="fas fa-solid fa-question"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `)
+})
+
+
 const usersInformationContainer = document.querySelector('tbody')
-
-users.forEach(user => {
+infos.lastUsers.forEach(user => {
     const date = new Date(user.createdAt);
     usersInformationContainer.insertAdjacentHTML('beforeend', `
     <tr>
